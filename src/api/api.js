f79d169489820d37347c16d4dfa9ecc0d58f2819
DarkKingpro10 <jesusgogeta@outlook.com>
2024-10-18 10:07:36 -0600

    refactor:Unificando productos y videojuegos en la API

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,11 @@
+import { STAR_WARS } from "./star-wars.js";
+import { VIDEOJUEGOS } from "./videojuegos.js";
+
 /*Función para setear los productos en el localStorage solo se ha de llamar
 una vez en caso de no haberse seteado antes.*/
 export default function setDB() {
+  const products = [...VIDEOJUEGOS, ...STAR_WARS]
+
   // localStorage.setItem('products', JSON.stringify());  
 }
 
